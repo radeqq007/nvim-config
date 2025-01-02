99a91cb9e73f8483d06ee332a9226999dbe97345
radeqq007 <radeqq007@interia.pl>
2025-01-02 12:51:52 +0100

    use spaces instead of tabs

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,9 @@
+-- Line numbers
 vim.opt.relativenumber = true
 vim.opt.number = true
 
+-- Use spaces instead of tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2 
