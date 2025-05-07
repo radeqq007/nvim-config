edcb497e1f6cafaf9e631ddd5564493a14126d92
radeqq007 <radeqq007@interia.pl>
2025-05-07 10:52:32 +0200

    feat: change color theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,6 +1,16 @@
 return {
-	"helbing/aura.nvim",
-	config = function() 
-		vim.cmd.colorscheme = "aura-theme"
+	"ellisonleao/gruvbox.nvim",
+	config = function ()
+    require("gruvbox").setup({
+      contrast = "soft",
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      underline_match_parentheses = true,
+      underline = true,
+      transparent_mode = true,
+    })
+    vim.cmd("colorscheme gruvbox")
 	end
 }
