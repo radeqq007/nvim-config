7c75b5820102b709b19c7d3a359a739ca363be93
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-02-19 20:47:53 +0100

    chore: move from depracted lsp config api

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,7 +26,6 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
       local servers = {
         "ts_ls", -- JavaScript/TypeScript
         "gopls", -- Go
@@ -40,7 +39,7 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       for _, server in ipairs(servers) do
-        lspconfig[server].setup({
+        vim.lsp.config(server, {
           capabilities = capabilities,
           settings = server == "lua_ls" and {
             Lua = {
@@ -51,33 +50,6 @@ return {
           } or {},
         })
       end
-    end,
-  },
-  -- Optional: Add nvim-cmp for autocompletion (recommended)
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "L3MON4D3/LuaSnip",
-    },
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-      })
     end,
   },
 }
