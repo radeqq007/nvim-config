a523b04b74c693a52678225ded2d4388bc961d0c
radeqq007 <radoslaw.kaczmarczyk@proton.me>
2026-07-01 20:19:50 +0200

    add lsp keybindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -51,6 +51,19 @@ return {
       end
 
       vim.lsp.enable(servers)
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local bufnr = args.buf
+          local opts = { buffer = bufnr }
+
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        end,
+      })
     end,
   },
 }
